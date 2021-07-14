290864bf3ff3930669cc8cb776b5c68c0fe844ae
RonaldCast <ronaldcastillo789@gmail.com>
2021-07-14 18:52:53 -0400

    implementation defineAsyncComponent

--- components-in-depth/src/App.vue
+++ components-in-depth/src/App.vue
@@ -1,40 +1,46 @@
 <template>
   <h1>Component in Depth</h1>
   <h2>Emite validate</h2>
-  <LoginForm @handler-form="getData" />
+  <LoginForm @handler-form="getData"  v-if="available" />
   <hr />
   <h2>Emite validate</h2>
-  <PersonalForm 
-   v-model:name.capitalize="name"
-   v-model:surname="surname" />
+  <PersonalForm v-model:name.capitalize="name" v-model:surname="surname" />
   {{ name }} {{ surname }}
   <h2>Provider / inject</h2>
   <ComponentParent />
+  <button @click="show">Show</button>
 </template>
 
 <script>
-import LoginForm from "./components/emitValidate/LoginForm.vue";
+//import LoginForm from "./components/emitValidate/LoginForm.vue";
 import PersonalForm from "./components/v-modelArguments/PersonalForm.vue";
 import ComponentParent from "./components/Provider/componentParent.vue";
-import { ref } from "vue";
+import { ref, defineAsyncComponent } from "vue";
 
 export default {
   name: "App",
   components: {
-    LoginForm,
+    LoginForm: defineAsyncComponent(() =>
+      import("./components/emitValidate/LoginForm.vue")
+    ),
     PersonalForm,
-    ComponentParent,
+    ComponentParent
   },
 
   setup() {
     const name = ref("");
     const surname = ref("");
+    const available = ref(false);
 
     const getData = (data) => {
       console.log(data);
     };
 
-    return { name, surname, getData };
+    const show = () => {
+      available.value = !available.value;
+    };
+
+    return { name, surname, getData, available, show };
   },
 };
 </script>
--- components-in-depth/src/components/Provider/componentParent.vue
+++ components-in-depth/src/components/Provider/componentParent.vue
@@ -1,42 +1,37 @@
 <template>
   <div>
-     <ComponentChild/>
-    <input v-model="description">
-    <button @click="add">
-      Add
-    </button>
+    <ComponentChild />
+    <input v-model="description" />
+    <button @click="add">Add</button>
   </div>
   <ul>
-    <li
-      v-for="task in listTask"
-      :key="task.id"
-    >
+    <li v-for="task in listTask" :key="task.id">
       {{ task }}
     </li>
   </ul>
 </template>
 <script>
 import { ref, provide, computed } from "vue";
-import ComponentChild from "./componentChild.vue"
+import ComponentChild from "./componentChild.vue";
 
 export default {
-  components:{
-    ComponentChild
+  components: {
+    ComponentChild,
   },
 
-  setup() {
+  async setup() {
     const description = ref("");
-     const listTask = ref([]); 
-    
-    provide("length",  computed(() =>listTask.value.length ) )
+    const listTask = ref([]);
+
+    provide(
+      "length",
+      computed(() => listTask.value.length)
+    );
     const add = () => {
-
-      listTask.value.push(description.value)
-      description.value = ""
-    }
-
-    return {description, listTask, add}
-  }, 
-  
+      listTask.value.push(description.value);
+      description.value = "";
+    };
+    return { description, listTask, add };
+  },
 };
 </script>
